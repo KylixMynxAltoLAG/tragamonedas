99360254c8c5e6e970ab5f24b1407584cb3ffc3b
vagrant <vagrant@kleer-csd-box>
2016-12-16 17:06:59 -0300

    imagenes

--- app.rb
+++ app.rb
@@ -8,10 +8,20 @@ end
 
 post '/entrar' do
     @@juego = Tragamonedas.new Jugador.new(params['nombre'], params['nivel'].to_i)
+
+    @p1 = "vacio.png"	
+    @p2 = "vacio.png"		
+    @p3 = "vacio.png"	
 	erb :tragamonedas
 end
 
 post '/jugar' do
     @@juego.jugar
+    @p1 = "#{@@juego.getTirada[0]}.png"	
+    @p2 = "#{@@juego.getTirada[1]}.png"	
+    @p3 = "#{@@juego.getTirada[2]}.png"	
     erb :tragamonedas
+
 end
+
+
